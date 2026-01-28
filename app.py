770d511ac86e2bec9e7172f46e9109130d48e136
volifnap123-lgtm <volifnap123@gmail.com>
2026-01-28 18:46:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -558,5 +558,5 @@ def keep_alive():
     return 'OK', 200
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+# if __name__ == '__main__':
+#    app.run(debug=True, host='0.0.0.0', port=5000)
